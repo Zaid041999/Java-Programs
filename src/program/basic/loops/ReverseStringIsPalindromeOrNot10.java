4a14a16b8be969aa8134f52d78bb38171665ff76
Zaid <zaid041999@gmail.com>
2024-10-15 20:19:02 +0530

    String palindrome optimsed

--- src/program/basic/loops/ReverseStringIsPalindromeOrNot10.java
+++ src/program/basic/loops/ReverseStringIsPalindromeOrNot10.java
@@ -3,17 +3,29 @@ package program.basic.loops;
 public class ReverseStringIsPalindromeOrNot10 {
     public static void main(String[] args) {
         String name ="mom";
-        String temp = name;
-        String rev ="";
-        int len =temp.length();
-        for (int i =len-1;i>=0;i--){
-            rev =rev + temp.charAt(i);
+        boolean isPalindrome=false;
+//        String temp = name;
+//        String rev ="";
+//        int len =temp.length();
+//        for (int i =len-1;i>=0;i--){
+//            rev =rev + temp.charAt(i);
+//        }
+//        if (rev.equals(temp)){
+//            System.out.println("Palindrome");
+//        }
+//        else {
+//            System.out.println("Not a Palindrome");
+//        }
+        int i=0;
+        int j =name.length()-1;
+        while(i<j){
+            if(name.charAt(i)!=name.charAt(j)){
+                isPalindrome =false;
+            }
+            isPalindrome=true;
+            i++;
+            j--;
         }
-        if (rev.equals(temp)){
-            System.out.println("Palindrome");
-        }
-        else {
-            System.out.println("Not a Palindrome");
-        }
+        System.out.println(isPalindrome);
     }
 }
