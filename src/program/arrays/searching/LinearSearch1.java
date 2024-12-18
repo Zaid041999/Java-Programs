617f938bab06a07aedb9aa91d67e55a2332641f1
Zaid <zaid041999@gmail.com>
2024-12-18 14:26:34 +0530

    LinearSearch

---
+++ src/program/arrays/searching/LinearSearch1.java
@@ -0,0 +1,39 @@
+package program.arrays.searching;
+
+import java.util.Scanner;
+
+public class LinearSearch1 {
+
+    public static int linearSearch(int []array,int target){
+        int index=-1;
+        for (int i = 0; i < array.length; i++) {
+            if(array[i]==target){
+                index=i;
+                break;
+            }
+
+        }
+        return index;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the the size of the Array :");
+        int size = sc.nextInt();
+        int []arr =new int[size];
+        System.out.println("Enter the element in the array one by one :");
+        for (int i = 0; i <arr.length ; i++) {
+                 arr[i] = sc.nextInt();
+        }
+        System.out.println("Enter the target element :");
+        int target = sc.nextInt();
+
+        //call the linearSearch()
+        int index = linearSearch(arr,target);
+        if (index==-1){
+            System.out.println("Element is not present int the array");
+        }else {
+            System.out.println("Element present at index :" + index);
+        }
+    }
+}
---
+++ src/program/arrays/searching/LinearSearch2.java
@@ -0,0 +1,42 @@
+package program.arrays.searching;
+
+import java.util.Scanner;
+
+public class LinearSearch2 {
+
+    public static int linearSearch(char []array,char target){
+
+        int index=-1;
+        for (int i = 0; i < array.length; i++) {
+            if(array[i]==target){
+                index=i;
+                break;
+            }
+
+        }
+        return index;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the the size of the Array :");
+        int size = sc.nextInt();
+        char []arr =new char[size];
+
+        System.out.println("Enter the element in the array one by one :");
+        for (int i = 0; i <size ; i++) {
+                 System.out.print("Character " + (i + 1) + ": ");
+                 arr[i] = sc.next().charAt(0);
+        }
+        System.out.println("Enter the target element :");
+        char target = sc.next().charAt(0);
+
+        //call the linearSearch()
+        int index = linearSearch(arr,target);
+        if (index==-1){
+            System.out.println("Element is not present int the array");
+        }else {
+            System.out.println("Element present at index :" + index);
+        }
+    }
+}
