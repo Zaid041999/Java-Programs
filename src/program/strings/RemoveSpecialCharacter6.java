95ab57fce462649ca8fc9c7bcfbf1174a1fe42b9
Zaid <zaid041999@gmail.com>
2025-01-08 11:38:03 +0530

    String

--- src/program/strings/RemoveSpecialCharacter6.java
+++ src/program/strings/RemoveSpecialCharacter6.java
@@ -2,8 +2,8 @@ package program.strings;
 
 public class RemoveSpecialCharacter6 {
     public static void main(String[] args) {
-        String s ="!Z#a&i*d)";
-        String str = s.replaceAll("[^a-bA-Z0-9]","");
+        String s ="!Z#ai&d)";
+        String str = s.replaceAll("[^a-z A-Z 0-9]","");
         System.out.println(str);
 
     }
